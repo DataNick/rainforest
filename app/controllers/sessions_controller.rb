301bc62f2f36d5ea1dc4d99b1787b8e9903cf854
DataNick <DataNick@users.noreply.github.com>
2014-11-11 17:40:59 -0500

    bug fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
   	user = User.find_by(email: params[:email])
-  	if user && ser.authenticated(params[:password])
+  	if user && user.authenticate(params[:password])
   		session[:user_id] = user.id
   		redirect_to products_url, notice: "Logged in!"
   	else
