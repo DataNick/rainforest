a71530465d59973b981050b7d6f39bf7f22f09b8
DataNick <DataNick@users.noreply.github.com>
2014-11-11 16:50:09 -0500

    Adding authorization to ensure users are logged in before they create reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-	before_filter :load_product
+  before_filter :ensure_logged_in, only: [:create, :destroy]
   def show
   	 @review = Review.find(params[:id])
   end
@@ -16,5 +16,16 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
+  	@review = Review.find(params[:id])
+    @review.destroy
+  end
+
+    private
+  def review_params
+    params.require(:review).permit(:comment, :product_id)
+  end
+
+  def load_product
+    @product = Product.find(params[:product_id])
   end
 end
